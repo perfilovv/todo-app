c90c15483258071436e588202d4c60d1a9a1c1df
perfilovv <russianspiritoneshotlol@gmail.com>
2023-04-11 16:22:34 +0300

    feat: add static todo form

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,14 @@
+import TodoForm from './components/TodoForm/TodoForm';
+
 import './App.css';
 
 function App() {
-    return <div className="app"></div>;
+    return (
+        <div className="app">
+            <h1 className="todo-app">Todo App</h1>
+            <TodoForm />
+        </div>
+    );
 }
 
 export default App;
---
+++ todo-app/src/components/TodoForm/TodoForm.js
@@ -0,0 +1,12 @@
+import './TodoForm.module.css';
+
+const TodoForm = () => {
+    return (
+        <>
+            <input type="text" placeholder="Enter new todo" />
+            <button>Submit</button>
+        </>
+    );
+};
+
+export default TodoForm;
