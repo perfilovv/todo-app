eb996b0d61f99b64c40bdb66d6993e24d578f159
perfilovv <russianspiritoneshotlol@gmail.com>
2023-04-12 12:51:04 +0300

    feat: add notification if array is empty

--- todo-app/src/components/TodoList/TodoList.js
+++ todo-app/src/components/TodoList/TodoList.js
@@ -4,6 +4,7 @@ import styles from './TodoList.module.css';
 const TodoList = ({ todos }) => {
     return (
         <div className={styles.todoListContainer}>
+            {!todos.length && <h2>Todo list is empty</h2>}
             {todos.map((todo, index) => (
                 <Todo key={index} todo={todo} />
             ))}
