9c4e8d63c6eb0dae874f405ac8ddc9f8c7699669
perfilovv <russianspiritoneshotlol@gmail.com>
2023-04-12 14:40:57 +0300

    feat: add react-icons in todo list

--- todo-app/src/components/Todo/Todo.js
+++ todo-app/src/components/Todo/Todo.js
@@ -1,8 +1,10 @@
+import { MdOutlineTaskAlt } from 'react-icons/md';
 import styles from './Todo.module.css';
 
 const Todo = ({ todo, index, deleteTodo }) => {
     return (
         <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+            <MdOutlineTaskAlt className={styles.todoIcon} />
             <div className={styles.todoText}>{todo}</div>
         </div>
     );
