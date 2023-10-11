905945b8da279396e5d557eb11f2b2ab4ec3f105
perfilovv <russianspiritoneshotlol@gmail.com>
2023-10-11 15:06:41 +0300

    feat: add check and delete buttons

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -21,11 +21,25 @@ function App() {
         setTodos(todos.filter((todo) => todo.id !== id));
     };
 
+    const toggleTodoHandler = (id) => {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id
+                    ? { ...todo, isCompleted: !todo.isCompleted }
+                    : { ...todo }
+            )
+        );
+    };
+
     return (
         <div className="app">
             <h1>Todo App</h1>
             <TodoForm addTodo={addTodoListener} />
-            <TodoList todos={todos} deleteTodo={deleteTodoListener} />
+            <TodoList
+                todos={todos}
+                deleteTodo={deleteTodoListener}
+                toggleTodo={toggleTodoHandler}
+            />
         </div>
     );
 }
--- todo-app/src/components/Todo/Todo.js
+++ todo-app/src/components/Todo/Todo.js
@@ -1,11 +1,23 @@
-import { RiTodoLine } from 'react-icons/ri';
+import { RiTodoLine, RiDeleteBin2Line, RiCheckLine } from 'react-icons/ri';
 import styles from './Todo.module.css';
 
-const Todo = ({ todo, deleteTodo }) => {
+const Todo = ({ todo, deleteTodo, toggleTodo }) => {
     return (
-        <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
+        <div
+            className={`${styles.todo} ${
+                todo.isCompleted ? styles.completedTodo : ''
+            }`}
+        >
             <RiTodoLine className={styles.todoIcon} />
             <div className={styles.todoText}>{todo.text}</div>
+            <RiDeleteBin2Line
+                className={styles.deleteIcon}
+                onClick={() => deleteTodo(todo.id)}
+            />
+            <RiCheckLine
+                className={styles.checkIcon}
+                onClick={() => toggleTodo(todo.id)}
+            />
         </div>
     );
 };
